f80f95a97b763413f8ae9ef501335e29e91010b0
Rodrigo Laviani <rodrigolaviani@gmail.com>
2020-12-30 22:38:00 -0300

    footer almost finished

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import '../assets/styles/components/Footer.scss'
+import Link from './Link';
+import contacts from '../data/footer.json'
 
-const Footer = () => (
-    <footer className="footer">
+const Footer = () => {
+    return (
+        <footer className="footer">
+            {
+                (contacts || []).map(contact => <Link key={contact.id} icon={contact.icon} name={contact.name} url={contact.url}></Link>)
+            }
+        </footer>
+    );
+};
 
-    </footer>
-);
 
 export default Footer;
---
+++ src/components/Link.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/styles/components/Link.scss'
+
+const Link = ({ name, icon, url }) => (
+    <div className="link">
+        <a href={url} target="_blank" rel="noopener noreferrer">
+            <img src={require(`../assets/images/${icon}`)} alt={name} />
+        </a>
+    </div>
+)
+
+export default Link;
